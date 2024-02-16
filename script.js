4d0fec7fa16de02dfe63c3d2f46275869f9f3678
eyadashraf123 <160035876+eyadashraf123@users.noreply.github.com>
2024-02-16 17:57:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// let form = document.querySelector("form");
+// let fname = document.getElementById("fname")
+// let email = document.getElementById("email")
+// let no = document.getElementById("no")
+// let Gov = document.getElementById("country")
+// let subject = document.getElementById("subject")
+// function sendEmail(){
+//     let bodyMessage = 
+//    `Full Name: ${fname.value}<br>
+//     Email: ${email.value}<br>
+//     Phone Number: ${no.value}<br>
+//     Country: ${country.value}<br>
+//     Full Name: ${subject.value}`
+//     Email.send({
+//         Host : "smtp.elasticemail.com",
+//         Username : "portfoliocontact21@gmail.com",
+//         Password : "13EB75D5730AC07E79728D230033551D3153",
+//         To : 'portfoliocontact21@gmail.com',
+//         From : "portfoliocontact21@gmail.com",
+//         Subject : subject.value,
+//         Body : bodyMessage
+//     }).then(
+//       message => alert(message)
+//     );
+// }
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     sendEmail();
+// })
